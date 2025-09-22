ed82c0a0c42e9331f4c82db9622968ca37abc6b0
Yasmin Oliveira <ycrisostomo051@gmail.com>
2025-09-22 09:19:51 -0300

    criação do delete

---
+++ queries/03-update.sql
@@ -0,0 +1,9 @@
+UPDATE usuarios SET username = 'ProGamer2025' 
+WHERE username = 'NoobMaster';
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET pontos = pontos + 1000;
+
+UPDATE usuarios SET email = 'main@gmail.com'
+WHERE email = 'master@email.com';
---
+++ queries/delete.sql
@@ -0,0 +1,14 @@
+DELETE FROM usuarios
+WHERE username = 'CheatCodeKing';
+
+SELECT * FROM usuarios;
+
+DELETE FROM usuarios
+WHERE id = 13;
+
+SELECT * FROM usuarios;
+
+DELETE FROM usuarios
+WHERE pontos < 2000
+
+SELECT * FROM usuarios;
